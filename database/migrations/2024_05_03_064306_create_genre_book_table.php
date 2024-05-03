07df84bc2dc57d1b096288347193560623b4a2f2
Bryan Januardo <bryan.januardo@binus.edu>
2024-05-03 14:06:54 +0700

    tes

---
+++ database/migrations/2024_05_03_064306_create_genre_book_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('genre_book', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('genre_id');
+            $table->unsignedBigInteger('ISBN');
+            $table->foreign('genre_id')->references('id')->on('genres')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('ISBN')->references('ISBN')->on('books')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('genre_book');
+    }
+};
