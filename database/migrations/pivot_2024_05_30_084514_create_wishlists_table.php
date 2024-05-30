a179d8dd92653788bfaff2b7c5ac6985990e13a8
Bryan Januardo <bryan.januardo@binus.edu>
2024-05-30 16:27:12 +0700

    Update Database Column

--- database/migrations/pivot_2024_05_30_084514_create_wishlists_table.php
+++ database/migrations/pivot_2024_05_30_084514_create_wishlists_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('wishlists', function (Blueprint $table) {
-            $table->id();
             $table->string('ISBN');
             $table->unsignedBigInteger('UserId');
             $table->foreign('ISBN')->references('ISBN')->on('books')->onDelete('cascade')->onUpdate('cascade');
