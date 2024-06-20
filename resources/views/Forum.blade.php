d5e8a39c2c566256a20161e1baaab912a8ac84c8
bunbunnibun <harleyputra778@gmail.com>
2024-06-20 21:46:26 +0700

    Discussion FE (navbar rusak gara gara bry)

--- resources/views/Forum.blade.php
+++ resources/views/Forum.blade.php
@@ -1,51 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Forum</title>
 
-    <link rel="stylesheet" href="{{asset('CSS/dashboard.css')}}">
+    <link rel="stylesheet" href="{{ asset('CSS/forum.css') }}">
 
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
     </style>
 
 </head>
+
 <body>
     @extends('Layout')
 
     @section('PageContent')
-    <div class="page-header">
-        <form action="">
-            <div class="genre-list">
-                <button id="all-genre-content" class="genre-content">All</button>
-                <button class="genre-content">Fiction</button>
-                <button class="genre-content">Non-Fiction</button>
+        <div class="page-header">
+            <div>
+                <form class="search-box" action="">
+                    <input placeholder="Search" type="text">
+                </form>
             </div>
-        </form>
-        <div>
-            <form class="search-box" action="">
-                <input placeholder="Search" type="text">
-            </form>
         </div>
-    </div>
 
-    <div class="page-content">
+        <div class="page-content">
 
-        <form action="">
-            <button class="post-card">
-                <div class="post-content">
-                    <h4 class="post-title"></h4>
-                    <p class="post-owner"></p>
-                    <p class="post-content"></p>
-                </div>
-            </button>
-        </form>
+            <form action="">
+                <button class="post-card">
+                    <div class="post-content">
+                        <h4 class="post-title">Judul Post</h4>
+                        <p class="post-owner">By "Pengarang"</p>
+                        <p class="post-body">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In optio nemo, atque praesentium asperiores et rem officia nobis sed fugiat. Totam ea accusamus alias temporibus assumenda, maiores veniam vitae explicabo porro quod architecto consectetur optio quaerat natus nulla molestiae, consequuntur illo ullam accusantium dolorem inventore? At cum officiis nam neque.</p>
+                    </div>
+                </button>
+            </form>
 
-    </div>
+        </div>
     @endsection
 
 
 </body>
+
 </html>
