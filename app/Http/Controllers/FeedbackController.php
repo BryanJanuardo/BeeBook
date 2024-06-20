f1cf92175b088680fb5b1da8de11540d5a44ebee
BryanJanuardo <bryan.januardo1401@gmail.com>
2024-06-20 19:39:00 +0700

    Fix Feedback

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -4,19 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Feedback;
+use Illuminate\Support\Facades\Auth;
 
 class FeedbackController extends Controller
 {
     public function store(Request $request, $ISBN)
     {
+        $user = Auth::user();
+        if($user == null){
+            return redirect()->route('Detail Book', ['ISBN' => $ISBN]);
+        }
+
         Feedback::create([
-            'UserId' => "12345",
+            'UserId' => $user->id,
             'ISBN' => $ISBN,
             'Subject' => $request->Subject,
             'Rating' => $request->Rating
         ]);
 
-        return redirect()->route('Dashboard');
+        return redirect()->route('Detail Book', ['ISBN' => $ISBN]);
     }
 
     public static function getAllFeedbackWithISBN($ISBN)
